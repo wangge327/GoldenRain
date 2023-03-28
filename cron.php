5c8608da329a91cb390dbb6bb39e0d7f239d08f7
wangge327 <wangge327@yahoo.com>
2023-03-29 01:10:13 +0800

    ads

--- cron.php
+++ cron.php
@@ -58,6 +58,10 @@ function delete_last_files($file_path){
             echo $full_file_path."<br>";
             delete_file($full_file_path);
         }
+        else{
+            echo $current_time."--".$compare_file_time."<br>";
+            echo $full_file_path."<br>";
+        }
     }
 }
 
